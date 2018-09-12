312377b9a12fb4103b482c85e54452f45b71620b
Dean Wagner <dean@dfwagners.net>
2018-09-12 19:04:20 -0400

    SUCCESSFUL transmission

--- videocall/public/js/main.js
+++ videocall/public/js/main.js
@@ -10,24 +10,18 @@ let remoteStream;
 
 let peerObj;
 let connectedUser = null;
-// let remotePeerConnection;
 
 // Define action buttons.
 const startButton = document.getElementById('startButton');
 const callButton = document.getElementById('callButton');
 const hangupButton = document.getElementById('hangupButton');
 
-
 callButton.disabled = true;
 hangupButton.disabled = true;
-
-
-
 
 startButton.addEventListener('click', startAction);
 callButton.addEventListener('click', callAction);
 hangupButton.addEventListener('click', hangupAction);
-
 
 
 function startAction() {
@@ -50,13 +44,19 @@ function startAction() {
   peerObj.addEventListener('icecandidate', function (event) {
     const iceCandidate = event.candidate;
     if (iceCandidate) {
-      const newIceCandidate = new RTCIceCandidate(iceCandidate);
-      peerObj.addIceCandidate(newIceCandidate)
-        .then(() => {
-          console.log("ice candidate success");
-        }).catch((error) => {
-          console.log("connection failure");
-        });
+      console.log(iceCandidate);
+      iceCandidate.type = "candidate";
+      sendMsg({"type": "candidate", "iceCan": iceCandidate});
+      // sendMsg(iceCandidate);
+      // const newIceCandidate = new RTCIceCandidate(iceCandidate);
+      // let condidateMsg = {"type": "candidate", "msg": iceCandidate}
+      // sendMsg(iceCandidate);
+      // peerObj.addIceCandidate(newIceCandidate)
+      //   .then(() => {
+      //     console.log("ice candidate success");
+      //   }).catch((error) => {
+      //     console.log("connection failure");
+      //   });
     }
   });
   peerObj.addEventListener('iceconnectionstatechange', function (event) {
@@ -80,21 +80,19 @@ function startAction() {
     console.log("Got message", data.type);
 
     switch(data.type) {
-      case "login":
+      case "login":               //  not used
          onLogin(data.success);
          break;
       case "offer":
          onOffer( data, msg.fm );
-         console.log(Object.keys(data));
-         console.log(data.sdp);
-         console.log(msg.fm);
          break;
       case "answer":
          onAnswer(data);
-         console.log(Object.keys(data));
          break;
       case "candidate":
-         onCandidate(data.candidate);
+         console.log("in candidate case");
+         console.log(data);
+         onCandidate(data);  //?????????
          break;
       default:
          break;
@@ -106,9 +104,6 @@ function callAction() {
   callButton.disabled = true;
   hangupButton.disabled = false;
   connectedUser = document.getElementById('remoteid').value;
-  console.log("peerObj ******************************");
-  console.log(peerObj);
-
   peerObj.createOffer({offerToReceiveVideo: 1,})
     .then((localdescription) => {
       peerObj.setLocalDescription(localdescription);
@@ -118,33 +113,22 @@ function callAction() {
 }
 
 function onOffer( sdp, fm ) {
-  // let offerObj = JSON.parse(document.getElementById('offerspace').value);
   console.log("in onOffer");
-  console.log(sdp)
   connectedUser = fm;
-  // let offerInitDict = JSON.parse(sdp);
-  // console.log(offerInitDict)
   peerObj.setRemoteDescription( sdp );
-  console.log(peerObj);
   peerObj.createAnswer(function (answer) {
-   console.log("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK");
     peerObj.setLocalDescription(answer);
-    console.log("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK");
-    console.log(answer);
+    console.log("sending answer");
     sendMsg(answer);
-
   }, function (error) {
     alert("oops...error");
   });
-
-
 }
 
 //when another user answers to our offer
 function onAnswer(answer) {
    peerObj.setRemoteDescription(answer);
 }
-
 
 function hangupAction() {
   peerObj.close();
@@ -154,9 +138,17 @@ function hangupAction() {
   console.log('Ending call.');
 }
 
+function onCandidate(data) {
+  const newIceCandidate = new RTCIceCandidate(data.iceCan);
+  peerObj.addIceCandidate(newIceCandidate)
+    .then(() => {
+      console.log("ice candidate success");
+    }).catch((error) => {
+      console.log("connection failure");
+    });
+}
+
 function sendMsg(msg) {
-  console.log("************************************************");
-  console.log(msg);
   var localnode = document.getElementById('localid').value;
   var nodeparams = {"msg": JSON.stringify(msg)}
   nodeparams.fm = localnode;
